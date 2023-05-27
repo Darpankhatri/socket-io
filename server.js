b7042c7383c38b0537c8d8d60d605a5bc89b444f
Darpan Kumar <darpankhatri817@gmail.com>
2023-05-27 22:02:30 +0500

    socket

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require('express');
 
 const app = express();
 
+
 const server = require('http').createServer(app);
 
-const io = require('socket.io')(server,{
-    cors: {origin: "*"}
+
+const io = require('socket.io')(server, {
+    cors: { origin: "*"}
 });
 
-server.listen(3000, ()=> {
-    console.log('server is running');
-})
+
+io.on('connection', (socket) => {
+    console.log('connection');
+    socket.on('joinChannel', (channel) => {
+        socket.join(channel); // Join the specified room/channel
+        console.log(channel);
+    });
+
+    socket.on('sendChatToServer', (message) => {
+        console.log(message);
+
+
+        // socket.broadcast.emit('sendChatToClient', message['message']);
+        io.to(message['channel']).emit('sendChatToClient', message['message']);
+    });
+
+
+    socket.on('disconnect', (socket) => {
+        console.log('Disconnect');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running');
+});
